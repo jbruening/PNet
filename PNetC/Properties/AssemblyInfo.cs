dd63fd994117f5c895c15b7087aaed6dcdf63496
unknown <robojoos@gmail.com>
2013-09-12 16:04:13 -0700

    *fixed assembly information

--- PNetC/Properties/AssemblyInfo.cs
+++ PNetC/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PNetC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PNetwork Client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PNetC")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Copyright © Justin Bruening")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- PNetU/Properties/AssemblyInfo.cs
+++ PNetU/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PNetU")]
-[assembly: AssemblyDescription("PNetwork for Unity")]
+[assembly: AssemblyDescription("PNetwork client binding for Unity")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PNetU")]
